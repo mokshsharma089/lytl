03e6a57d347f8541e4e0b068401225f4dd303481
Moksh Sharma <mokshsharma@Mokshs-MacBook-Air.local>
2020-07-26 08:53:58 +0530

    try to change heroku master

--- rickUrl/settings.py
+++ rickUrl/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -84,16 +85,11 @@ WSGI_APPLICATION = 'rickUrl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hleqpeny',
-        'USER':'hleqpeny',
-        'PASSWORD':'WyWbtoiR2NViPnmvkJlzlUKD_13Vi8Ie',
-        'HOST':'satao.db.elephantsql.com',
-        'PORT':'5432'
-    }
+
+DATABASES={'default': {'ENGINE': 'django.db.backends.postgresql','NAME': 'hleqpeny','USER':'hleqpeny','PASSWORD':'WyWbtoiR2NViPnmvkJlzlUKD_13Vi8Ie','HOST':'satao.db.elephantsql.com', 'PORT':'5432'
+    }
 }
+
 
 
 # Password validation
@@ -133,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
