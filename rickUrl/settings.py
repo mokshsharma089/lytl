8d233a57e16b6d1cec21e7f2ef5924eb962d02af
Moksh Sharma <mokshsharma@Mokshs-MacBook-Air.local>
2020-07-26 09:32:58 +0530

    put postgres elephant sql again

--- rickUrl/settings.py
+++ rickUrl/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y_ci26@$so^^)85d@spj3*d+_bx_1ux318t2t^*4@n-jst10h1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['teeensy.herokuapp.com','127.0.0.1']
 
@@ -86,10 +86,17 @@ WSGI_APPLICATION = 'rickUrl.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-DATABASES={'default': {'ENGINE': 'django.db.backends.postgresql','NAME': 'hleqpeny','USER':'hleqpeny','PASSWORD':'WyWbtoiR2NViPnmvkJlzlUKD_13Vi8Ie','HOST':'satao.db.elephantsql.com', 'PORT':'5432'
-    }
-}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hleqpeny',
+        'USER': 'hleqpeny',
+        'PASSWORD':'WyWbtoiR2NViPnmvkJlzlUKD_13Vi8Ie',
+        'HOST':'satao.db.elephantsql.com',
+        'PORT':'5432',
 
+    }
+}
 
 
 # Password validation
