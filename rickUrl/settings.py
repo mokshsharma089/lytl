dc05bb183ea59e026fafd59183b5f86a21318550
Moksh Sharma <mokshsharma@Mokshs-MacBook-Air.local>
2020-07-28 16:33:44 +0530

    hey does this work on tuesday

--- rickUrl/settings.py
+++ rickUrl/settings.py
@@ -85,16 +85,15 @@ WSGI_APPLICATION = 'rickUrl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hleqpeny',
-        'USER':'hleqpeny',
-        'PASSWORD':'WyWbtoiR2NViPnmvkJlzlUKD_13Vi8Ie',
-        'HOST':'satao.db.elephantsql.com',
-        'PORT':'5432'
-    }
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'hleqpeny',
+        'USER':'hleqpeny',
+        'PASSWORD':'WyWbtoiR2NViPnmvkJlzlUKD_13Vi8Ie',
+        'HOST':'satao.db.elephantsql.com',
+        'PORT':'5432'
+    }
 }
 
 
--- shortner/forms.py
+++ shortner/forms.py
@@ -3,4 +3,4 @@ from.validators import validate_url,validate_dot_com
 
 
 class SubmitUrlForm(forms.Form):
-    url = forms.CharField(widget=forms.TextInput(),label="Submit long URL")
+    url = forms.CharField(widget=forms.TextInput(),max_length=1024,label="Submit long URL")
